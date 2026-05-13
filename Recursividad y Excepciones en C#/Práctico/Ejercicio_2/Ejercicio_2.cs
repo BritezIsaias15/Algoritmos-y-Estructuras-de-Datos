1e57beed9c75391992fa5c2114743b5134e0e5bd
BritezIsaias15 <britezisaias152@gmail.com>
2026-05-13 09:48:53 -0300

    Add files via upload

---
+++ Recursividad y Excepciones en C#/Práctico/Ejercicio_2/Ejercicio_2.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+try
+{
+    Console.WriteLine("Ingrese su número base");
+    int basee = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese su número exponente");
+    int exponente = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("El resultado es " + calcularPotencia(basee, exponente));
+}
+catch (FormatException)
+{
+    Console.WriteLine("Error: El número ingresado no es válido.");
+}
+static int calcularPotencia(int basee, int exponente)
+{
+    if (exponente == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return basee * calcularPotencia(basee, exponente - 1);
+    }
+}
