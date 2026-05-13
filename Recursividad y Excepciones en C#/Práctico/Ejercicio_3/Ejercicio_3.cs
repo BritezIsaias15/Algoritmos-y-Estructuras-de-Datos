20ab7bd6026f2281cadc6db0c2f9046affa18053
BritezIsaias15 <britezisaias152@gmail.com>
2026-05-13 09:50:48 -0300

    Add files via upload

---
+++ Recursividad y Excepciones en C#/Práctico/Ejercicio_3/Ejercicio_3.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection.Metadata.Ecma335;
+Console.WriteLine("Ingrese el número a invertir");
+int.TryParse(Console.ReadLine(), out int n);
+
+invertirNumero(n);
+
+static int invertirNumero(int n)
+{
+    if (n < 10)
+    {
+        Console.Write(n);
+        return n;
+    }
+    else
+    {
+        Console.Write(n % 10);
+        return invertirNumero(n / 10);
+    }
+}
+
