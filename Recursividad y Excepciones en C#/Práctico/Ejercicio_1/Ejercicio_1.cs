4af5f8d02fe73268a864e80b0236df93ecab585c
BritezIsaias15 <britezisaias152@gmail.com>
2026-05-13 09:45:26 -0300

    Add files via upload

---
+++ Recursividad y Excepciones en C#/Práctico/Ejercicio_1/Ejercicio_1.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Ingrese el número a descomponer:");
+int.TryParse(Console.ReadLine(), out int n);
+
+contarNumeros(n);
+static int contarNumeros(int n)
+{
+    if (n < 10)
+    {
+        return 1;
+    }
+    else
+    {
+        return 1 + contarNumeros(n / 10);
+    }
+}
+
+Console.WriteLine("El número tiene " + contarNumeros(n) + " dígitos.");
