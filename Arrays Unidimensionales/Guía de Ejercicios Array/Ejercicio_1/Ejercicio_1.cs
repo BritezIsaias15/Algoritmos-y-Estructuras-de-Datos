61dd56365da9c9104a92b90cea1fb3bd91e2a614
BritezIsaias15 <britezisaias152@gmail.com>
2026-05-29 09:36:45 -0300

    Update Ejercicio_1.cs

--- Arrays Unidimensionales/Guía de Ejercicios Array/Ejercicio_1/Ejercicio_1.cs
+++ Arrays Unidimensionales/Guía de Ejercicios Array/Ejercicio_1/Ejercicio_1.cs
@@ -1,1 +1,29 @@
+int[] arreglo1 = { 1, 2, 3, 4 };
+int[] arreglo2 = { 1, 2, 3, 4 };
+int producto = productoEscalarConInverso(arreglo1, arreglo2);
+Console.WriteLine($"El producto escalar entre el arreglo 1 y el inverso del arreglo 2 es: {producto}");
+
+
+static int productoEscalarConInverso(int[] arreglo1, int[] arreglo2)
+{
+    int[] arreglo2Inv = new int[arreglo2.Length];
+    int j = 0;
+    int resultado = 0;
+    int producto = 0;
+
+    //Invertir arreglo2
+    for (int i = arreglo2.Length - 1; i >= 0; i--)
+    {
+        arreglo2Inv[j] = arreglo2[i];
+        j++;
+    }
+
+    //Conseguir el producto
+    for (int k = 0; k < arreglo1.Length; k++)
+    {
+        producto += (arreglo1[k] * arreglo2Inv[k]);
+    }
+    return producto;
+}
+
 
