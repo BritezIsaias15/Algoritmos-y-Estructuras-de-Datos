ea57c0e740bb4556a4cc522c85289478c4a06518
BritezIsaias15 <britezisaias152@gmail.com>
2026-05-29 09:39:26 -0300

    Update Ejercicio_2.cs

--- Arrays Unidimensionales/Guía de Ejercicios Array/Ejercicio_2/Ejercicio_2.cs
+++ Arrays Unidimensionales/Guía de Ejercicios Array/Ejercicio_2/Ejercicio_2.cs
@@ -1,1 +1,28 @@
+using System.Timers;
+
+int[] arreglo1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int buscar = 6;
+int resultado = busquedaL(arreglo1, buscar);
+
+if (resultado < 0)
+{
+    Console.WriteLine("No se encotró el número en el array.");
+}
+else
+{
+    Console.WriteLine($"El número {buscar} se encuentra en la posición {resultado} del array.");
+}
+
+static int busquedaL(int[] arreglo1, int buscar)
+{
+    int resultado = -1;
+    for (int i = 0; i < arreglo1.Length; i++)
+    {
+        if (arreglo1[i] == buscar)
+        {
+            return i;
+        }
+    }
+    return resultado;
+}
 
