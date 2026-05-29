052b07d673ded15506f0d4531ddc6365d7ee1061
BritezIsaias15 <britezisaias152@gmail.com>
2026-05-29 09:31:05 -0300

    Add files via upload

---
+++ Arrays Unidimensionales/Arreglos en C# Apuntes/Ejercicio_5/Ejercicio_5.cs
@@ -0,0 +1,25 @@
+﻿int[] num = { 23, 75, 4, 12, 43, 62, 77, 81, 9, 90 };
+int buscar = 43;
+
+int posicion = busquedaLineal(num, buscar);
+
+if (posicion != -1)
+{
+    Console.WriteLine($"El número {buscar} se encuentra en la posición {posicion} del array.");
+}
+else
+{
+    Console.WriteLine("El número no se encuentra en el array.");
+}
+
+    static int busquedaLineal(int[] num, int buscar)
+    {
+        for (int i = 0; i < num.Length; i++)
+        {
+            if (num[i] == buscar)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
