5b33dc4ac0f0eda6ace8ea83a87ddad5173ec08d
BritezIsaias15 <britezisaias152@gmail.com>
2026-05-29 09:05:19 -0300

    Add files via upload

---
+++ Arrays Unidimensionales/Arreglos en C# Apuntes/Ejercicio_3/Ejercicio_3.cs
@@ -0,0 +1,23 @@
+﻿int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+int[] reverso = reversa(num);
+
+Console.WriteLine("Array al reverso");
+foreach (int numeros in reverso)
+{
+    Console.WriteLine(numeros + " ");
+}
+
+static int[] reversa(int[] num)
+{
+    int N = num.Length;
+    int[] resultado = new int[N];
+    int j = 0;
+
+    for (int i = N - 1; i >= 0; i--)
+    {
+        resultado[j] = num[i];
+        j++;
+    }
+    return resultado;
+}
