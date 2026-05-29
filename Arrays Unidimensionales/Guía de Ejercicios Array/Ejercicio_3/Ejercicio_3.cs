9f792d40c33412203eb0279d87b30b7a3dbc3ba1
BritezIsaias15 <britezisaias152@gmail.com>
2026-05-29 09:41:08 -0300

    Update Ejercicio_3.cs

--- Arrays Unidimensionales/Guía de Ejercicios Array/Ejercicio_3/Ejercicio_3.cs
+++ Arrays Unidimensionales/Guía de Ejercicios Array/Ejercicio_3/Ejercicio_3.cs
@@ -1,1 +1,25 @@
+int[] arreglo1 = { 7, 2, 9, 1, 3, 6, 4, 8, 10, 5 };
+int[] resultado = ordenar(arreglo1);
+
+foreach (int numeros in resultado)
+{
+    Console.WriteLine(numeros);
+}
+
+static int[] ordenar(int[] arreglo1)
+{
+    for (int i = 0; i < arreglo1.Length - i - 1; i++)
+    {
+        for (int j = 0; j < arreglo1.Length - 1; j++)
+        {
+            if (arreglo1[j] > arreglo1[j + 1]) //Si el primer numero es mayor que el siguiente y siguiente y siguiente...
+            {
+                int auxiliar = arreglo1[j];
+                arreglo1[j] = arreglo1[j + 1];
+                arreglo1[j + 1] = auxiliar;
+            }
+        }
+    }
+    return arreglo1;
+}
 
