e9f48dc95f83089ff885a1db3a080dcc01cfc34f
BritezIsaias15 <britezisaias152@gmail.com>
2026-05-29 09:01:15 -0300

    Add files via upload

---
+++ Arrays Unidimensionales/Arreglos en C# Apuntes/Ejercicio_2/Ejercicio_2.cs
@@ -0,0 +1,24 @@
+﻿int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int[] b = { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+int[] resultado = productoDeArreglos(a, b);
+
+Console.WriteLine("Arreglo c");
+foreach (int numeros in resultado)
+{
+    Console.WriteLine(numeros);
+}
+
+static int[] productoDeArreglos(int[] a, int[] b)
+{
+    int N = a.Length;
+
+    int[] c = new int[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        c[i] = a[i] * b[N - (i + 1)];
+    }
+    return c;
+}
+
