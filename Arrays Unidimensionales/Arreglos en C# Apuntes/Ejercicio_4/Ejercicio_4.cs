80fbab495aaaf4f67ee0bd85161331e339cdc154
BritezIsaias15 <britezisaias152@gmail.com>
2026-05-29 09:26:45 -0300

    Add files via upload

---
+++ Arrays Unidimensionales/Arreglos en C# Apuntes/Ejercicio_4/Ejercicio_4.cs
@@ -0,0 +1,21 @@
+﻿int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+double[] porPi = multiplicarPorPi(num);
+
+Console.WriteLine("Array por pi");
+foreach (double numeros in porPi)
+{
+    Console.WriteLine(numeros + " ");
+}
+
+static double[] multiplicarPorPi(int[] num)
+{
+    int N = num.Length;
+    double[] resultado = new double[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        resultado[i] = i * 3.14;
+    }
+    return resultado;
+}
