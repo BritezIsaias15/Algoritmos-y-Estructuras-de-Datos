9ded3c9e25f7778dd06825866f65ed54f259ba50
BritezIsaias15 <britezisaias152@gmail.com>
2026-06-07 11:59:21 -0300

    Update Ejercicio.cs

--- Arreglos - Ejercicios para practicar en clase/Nivel 3/Ejercicio 3.1/Ejercicio.cs
+++ Arreglos - Ejercicios para practicar en clase/Nivel 3/Ejercicio 3.1/Ejercicio.cs
@@ -1,31 +1,37 @@
-Console.WriteLine("¿Cuántos números desea ingresar?");
-int.TryParse(Console.ReadLine(), out int cant);
-Console.Clear();
-
-int[] array = new int[cant];
+int[] array = new int[5];
 
 for (int i = 0; i < array.Length; i++)
 {
-    Console.WriteLine($"Ingrese el N°{i+1}.");
+    Console.WriteLine($"Ingrese el N°{i + 1}.");
     int.TryParse(Console.ReadLine(), out array[i]);
     Console.Clear();
 }
 
-foreach(int numeros in array)
+Console.WriteLine("¿Cuántas veces desea rotar?");
+int.TryParse(Console.ReadLine(), out int rotar);
+rotar = rotar % 5;
+
+int[] rotado = Rotar(array, rotar);
+
+foreach (int numeros in array)
 {
     Console.Write(numeros + " ");
 }
-
-Console.WriteLine("¿Cuántas veces desea rotar?");
-int.TryParse(Console.ReadLine(), out int rotar);
-
 int[] Rotar(int[] array, int rotar)
 {
-    int auxiliar;
-    int auxiliar2;
-    for (int i = 0; i <= rotar; i++)
+    int aux;
+    int aux2;
+    for (int i = 0; i < rotar; i++)
     {
-        
+        aux = array[1];
+        array[1] = array[0];
+        aux2 = array[2];
+        array[2] = aux;
+        aux = array[3];
+        array[3] = aux2;
+        aux2 = array[4];
+        array[4] = aux;
+        array[0] = aux2;
     }
     return array;
 }
