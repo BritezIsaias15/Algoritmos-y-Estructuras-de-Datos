acdece075850f4056755cae756cbe3e8ecedb7de
BritezIsaias15 <britezisaias152@gmail.com>
2026-06-08 11:20:50 -0300

    Update Ejercicio.cs

--- Arreglos - Ejercicios para practicar en clase/Nivel 3/Ejercicio 3.2/Ejercicio.cs
+++ Arreglos - Ejercicios para practicar en clase/Nivel 3/Ejercicio 3.2/Ejercicio.cs
@@ -1,20 +1,55 @@
-using System.Reflection.Metadata.Ecma335;
-
 int[] array = { 1, 2, 2, 3, 4, 5, 6, 6, 6, 7, 8, 9, 9, 10 };
 
 Console.WriteLine($"Array actual:");
-foreach(int numeros in array)
+foreach (int numeros in array)
 {
     Console.Write(numeros + " ");
 }
 
-int[] Duplicados(int[] array)
+Console.WriteLine();
+
+int[] sinRepetidos = new int[longitudNueva(array)];
+
+int[] nuevoArray = arrayCorregido(sinRepetidos, array);
+
+Console.WriteLine($"Array corregido:");
+foreach (int numeros in nuevoArray)
 {
+    Console.Write(numeros + " ");
+}
+
+int[] arrayCorregido(int[] sinRepetidos, int[] array)
+{
+    if (array.Length == 0) return sinRepetidos;
+        
+    int j = 0;
+
     for (int i = 0; i < array.Length - 1; i++)
     {
-        if (array[i] == array[i+1])
+        if (array[i] != array[i + 1])
         {
-            
+            sinRepetidos[j] = array[i];
+            j++;
         }
     }
+    sinRepetidos[j] = array[array.Length - 1];
+    return sinRepetidos;
+}
+
+int longitudNueva(int[] array)
+{
+    if (array.Length == 0) return 0;
+
+    int j = 1;
+
+    for (int i = 0; i < array.Length - 1 ; i++)
+    {
+        if (array[i] != array[i + 1])
+        {
+            j++;
+        }
+        
+    }
+    
+    return j;
 }
