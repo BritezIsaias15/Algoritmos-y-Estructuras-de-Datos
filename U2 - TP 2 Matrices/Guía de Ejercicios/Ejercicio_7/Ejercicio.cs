45c99875cb4f16724ea3e3f2ea388c168c2232de
BritezIsaias15 <britezisaias152@gmail.com>
2026-06-19 09:49:54 -0300

    Create Ejercicio.cs

---
+++ U2 - TP 2 Matrices/Guía de Ejercicios/Ejercicio_7/Ejercicio.cs
@@ -0,0 +1,33 @@
+int[,] matriz = new int[3, 3]
+{
+    {7, 2, 4},
+    {1, 1, 3},
+    {8, 9, 5}
+};
+
+var(fila, columna) = Maximo(matriz);
+
+Console.WriteLine($"El mayor elemento de la matriz se encuentra en la fila {fila} y la columna {columna}.");
+
+(int fila, int columna) Maximo(int[,] matriz)
+{
+    int fila = matriz.GetLength(0);
+    int columna = matriz.GetLength(1);
+    int max = 0;
+    int posColum = 0;
+    int posFila = 0;
+
+    for (int i = 0; i < fila; i++)
+    {
+        for (int j= 0; j < columna; j++)
+        {
+            if (matriz[i,j] > max)
+            {
+                max = matriz[i, j];
+                posFila = i;
+                posColum = j;
+            }
+        }
+    }
+    return (posFila, posColum);
+}
