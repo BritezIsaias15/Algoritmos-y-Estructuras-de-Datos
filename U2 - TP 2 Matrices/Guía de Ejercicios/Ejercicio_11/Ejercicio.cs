9672707aba0224622d90dcc14d9c6c328ada3e34
BritezIsaias15 <britezisaias152@gmail.com>
2026-06-21 17:35:05 -0300

    Update Ejercicio.cs

--- U2 - TP 2 Matrices/Guía de Ejercicios/Ejercicio_11/Ejercicio.cs
+++ U2 - TP 2 Matrices/Guía de Ejercicios/Ejercicio_11/Ejercicio.cs
@@ -4,27 +4,71 @@ int[,] matriz = new int[3, 3]
     {1, 1, 3},
     {8, 9, 5}
 };
-
-int[,] matrizInvertida = Intercambio(matriz);
-
-
-int[,] Intercambio(int[,] matriz)
+try
 {
-    Console.WriteLine("Ingrese la primera fila para intercambiar. (0 - 2)");
-    int.TryParse(Console.ReadLine(), out int priFila);
-    Console.WriteLine("Ingrese la segunda fila para intercambiar. (0 - 2)");
-    int.TryParse(Console.ReadLine(), out int secFila);
-    int columna = matriz.GetLength(1);
+    matriz = MostrarMatriz(matriz);
 
-    if (priFila == secFila)
+    int[,] MostrarMatriz(int[,] matriz)
     {
+        Console.Write($"Matriz actual:");
+
+        for (int i = 0; i < matriz.GetLength(0); i++)
+        {
+            Console.WriteLine();
+            for (int j = 0; j < matriz.GetLength(1); j++)
+            {
+                Console.Write(matriz[i, j] + " ");
+            }
+        }
+        Console.WriteLine();
+
+        matriz = Intercambio(matriz);
+
+        Console.Write($"Matriz invertida:");
+
+        for (int i = 0; i < matriz.GetLength(0); i++)
+        {
+            Console.WriteLine();
+            for (int j = 0; j < matriz.GetLength(1); j++)
+            {
+                Console.Write(matriz[i, j] + " ");
+            }
+        }
         return matriz;
     }
-    else
+
+    int[,] Intercambio(int[,] matriz)
     {
-        for (int i = 0; i < columna; i++)
+        Console.WriteLine("Ingrese la primera fila para intercambiar. (1 - 3)");
+        int priFila = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese la segunda fila para intercambiar. (1 - 3)");
+        int secFila = Convert.ToInt32(Console.ReadLine());
+        int columna = matriz.GetLength(1);
+        priFila--;
+        secFila--;
+        int auxiliar;
+
+        if (priFila == secFila)
         {
-            int[] auxiliar = matriz[priFila, i];
+            return matriz;
         }
+        else
+        {
+            for (int i = 0; i < columna; i++)
+            {
+                auxiliar = matriz[priFila, i];
+                matriz[priFila, i] = matriz[secFila, i];
+                matriz[secFila, i] = auxiliar;
+            }
+        }
+        return matriz;
     }
+}
+catch (FormatException) 
+{
+    Console.WriteLine("Formato no válido.");
+}
+catch (IndexOutOfRangeException)
+{
+    Console.WriteLine("Fuera de rango.");
 }
