8e069cc55b92339c00624c884bfa38726eb405da
BritezIsaias15 <britezisaias152@gmail.com>
2026-08-07 11:56:51 -0300

    Update Ejercicio.cs

--- Pilas (Stacks)/Ejercicio_3/Ejercicio.cs
+++ Pilas (Stacks)/Ejercicio_3/Ejercicio.cs
@@ -1,19 +1,1 @@
-Console.WriteLine("Seleccione una opción \n1.Ir a página\t 2.Atrás");
-string texto = Console.ReadLine();
 
-if (texto == "")
-{
-    Console.WriteLine("No ha ingresado nada.");
-}
-else
-{
-    var result = Navegar(texto);
-
-}
-
-string Navegar(string texto)
-{
-    var pila = new Stack<string>();
-
-
-}
