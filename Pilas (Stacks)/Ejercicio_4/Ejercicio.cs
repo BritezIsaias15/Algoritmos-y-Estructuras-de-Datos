e3d8093d31181327e701c5430f6b33d6e70e8f12
BritezIsaias15 <britezisaias152@gmail.com>
2026-08-10 12:34:46 -0300

    Create Ejercicio.cs

---
+++ Pilas (Stacks)/Ejercicio_4/Ejercicio.cs
@@ -0,0 +1,11 @@
+using System;
+class HelloWorld {
+  static void Main() {
+      while (true)
+      {
+          Console.WriteLine("Seleccione una opción \t 1.Escribir \t 2.Deshacer \t 3.Mostrar");
+          
+         
+      }
+  }
+}
