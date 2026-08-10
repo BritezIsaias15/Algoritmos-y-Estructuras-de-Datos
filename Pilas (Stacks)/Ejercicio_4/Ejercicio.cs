0e6fc65705a5b016098a34fc8cda1ced544a31f5
BritezIsaias15 <britezisaias152@gmail.com>
2026-08-10 12:40:59 -0300

    Update Ejercicio.cs

--- Pilas (Stacks)/Ejercicio_4/Ejercicio.cs
+++ Pilas (Stacks)/Ejercicio_4/Ejercicio.cs
@@ -1,11 +1,25 @@
 using System;
 class HelloWorld {
   static void Main() {
+      var texto = new stack<string>();
+      
       while (true)
       {
-          Console.WriteLine("Seleccione una opción \t 1.Escribir \t 2.Deshacer \t 3.Mostrar");
+          Console.WriteLine("Seleccione una opción \n 1.Escribir \t 2.Deshacer \t 3.Mostrar");
+          string input = Console.ReadLine();
           
-         
+          switch(input)
+          {
+              case 1:
+              Escribir();
+          }
       }
+      
   }
+  public string Escribir()
+  {
+      Console.WriteLine("Ingrese el texto.")
+      texto.Push(Console.ReadLine());
+  }
+  
 }
