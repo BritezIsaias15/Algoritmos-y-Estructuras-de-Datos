04c79f3c58a2d6e3eacfe74ee0b186c95887e1b2
BritezIsaias15 <britezisaias152@gmail.com>
2026-08-10 11:18:27 -0300

    Update Ejercicio.cs

--- Pilas (Stacks)/Ejercicio_3/Ejercicio.cs
+++ Pilas (Stacks)/Ejercicio_3/Ejercicio.cs
@@ -1,1 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
+namespace HelloWorld
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+		    var paginas = new Stack<string>();
+			while (true)
+			{
+				Console.WriteLine("Escriba ir a página o atrás.");
+				string opcion = Console.ReadLine();
+
+				if (opcion == "Ir a página")
+				{
+					Console.WriteLine("Ingrese la URL.");
+					paginas.Push(Console.ReadLine());
+				}
+				if (opcion == "Atrás")
+				{
+					if (paginas.Count() !=0)
+					{
+						Console.WriteLine(paginas.Pop());
+					}
+					else
+					{
+						Console.WriteLine("No hay páginas anteriores.");
+					}
+				}
+			}
+		}
+	}
+}
