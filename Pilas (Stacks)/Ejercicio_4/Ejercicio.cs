796cf3e2e765895bcb777d21ddf8c36115d1d0c6
BritezIsaias15 <britezisaias152@gmail.com>
2026-08-10 21:59:44 -0300

    Update Ejercicio.cs

--- Pilas (Stacks)/Ejercicio_4/Ejercicio.cs
+++ Pilas (Stacks)/Ejercicio_4/Ejercicio.cs
@@ -1,25 +1,55 @@
-using System;
-class HelloWorld {
-  static void Main() {
-      var texto = new stack<string>();
-      
-      while (true)
-      {
-          Console.WriteLine("Seleccione una opción \n 1.Escribir \t 2.Deshacer \t 3.Mostrar");
-          string input = Console.ReadLine();
-          
-          switch(input)
-          {
-              case 1:
-              Escribir();
-          }
-      }
-      
-  }
-  public string Escribir()
-  {
-      Console.WriteLine("Ingrese el texto.")
-      texto.Push(Console.ReadLine());
-  }
-  
+
+var texto = new List<string>();
+
+while (true)
+{
+    Console.WriteLine("Seleccione una opción \n1.Escribir \t 2.Deshacer \t 3.Mostrar");
+    int input = int.Parse(Console.ReadLine());
+
+    switch (input)
+    {
+        case 1:
+            Escribir();
+            break;
+        case 2:
+            Deshacer();
+            break;
+        case 3:
+            Mostrar();
+            break;
+        default:
+            Console.WriteLine("Ingrese una opción válida");
+            break;
+         
+    }
+}
+
+void Mostrar()
+{
+    Console.Clear();
+    foreach (string word in texto)
+    {
+        Console.Write(word);
+    }
+    Console.WriteLine();
+}
+
+void Deshacer()
+{
+    Console.Clear();
+    if (texto.Count() == 0)
+    {
+        Console.WriteLine("No hay texto el cuál deshacer.");
+    }
+    else
+    {
+        texto.RemoveAt(texto.Count()-1);
+    }
+}
+
+void Escribir()
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese el texto.");
+    texto.Add(Console.ReadLine());
 }
